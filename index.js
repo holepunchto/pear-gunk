da105a8b2b14aba8b59eeee659efd1e0a337629f
Mikkel Malmberg <mikkel@brnbw.com>
2025-09-26 18:11:12 +0200

    Migrate to Prettier (#3)
    * Migrate to Prettier
    
    * Apply Prettier

--- index.js
+++ index.js
@@ -17,44 +17,89 @@ const bmap = isBare
   : {}
 
 const overrides = [
-  'pear', 'electron',
-  'crc-universal', 'quickbit-universal', 'sodium-native', 'udx-native', 'rocksdb-native',
-  'fs-native-extensions', 'assert', 'console', 'events', 'fs', 'fs/promises', 'http', 'os',
-  'path', 'child_process', 'repl', 'url', 'tty', 'module', 'process', 'timers', 'inspector',
+  'pear',
+  'electron',
+  'crc-universal',
+  'quickbit-universal',
+  'sodium-native',
+  'udx-native',
+  'rocksdb-native',
+  'fs-native-extensions',
+  'assert',
+  'console',
+  'events',
+  'fs',
+  'fs/promises',
+  'http',
+  'os',
+  'path',
+  'child_process',
+  'repl',
+  'url',
+  'tty',
+  'module',
+  'process',
+  'timers',
+  'inspector',
   'crypto'
 ]
 
 const builtins = {
-  has (ns) {
+  has(ns) {
     return overrides.includes(ns) || defaults.builtins.has(ns)
   },
-  get (ns) {
+  get(ns) {
     switch (ns) {
-      case 'crc-universal': return require('crc-universal')
-      case 'quickbit-universal': return require('quickbit-universal')
-      case 'sodium-native': return require('sodium-native')
-      case 'udx-native': return require('udx-native')
-      case 'fs-native-extensions': return require('fs-native-extensions')
-      case 'rocksdb-native': return require('rocksdb-native')
-      default: return Object.hasOwn(bmap, ns) ? require(bmap[ns]) : defaults.builtins.get(ns)
+      case 'crc-universal':
+        return require('crc-universal')
+      case 'quickbit-universal':
+        return require('quickbit-universal')
+      case 'sodium-native':
+        return require('sodium-native')
+      case 'udx-native':
+        return require('udx-native')
+      case 'fs-native-extensions':
+        return require('fs-native-extensions')
+      case 'rocksdb-native':
+        return require('rocksdb-native')
+      default:
+        return Object.hasOwn(bmap, ns)
+          ? require(bmap[ns])
+          : defaults.builtins.get(ns)
     }
   },
-  keys () { return Array.from(new Set([...defaults.builtins.keys(), ...overrides])) }
+  keys() {
+    return Array.from(new Set([...defaults.builtins.keys(), ...overrides]))
+  }
 }
 
 const bareBuiltins = {
-  has (ns) { return ns === 'electron' || defaults.builtins.has(ns) },
-  get (ns) { return ns === 'electron' ? builtins.get(ns) : defaults.builtins.get(ns) },
-  keys () { return [...defaults.builtins.keys(), 'electron'] }
+  has(ns) {
+    return ns === 'electron' || defaults.builtins.has(ns)
+  },
+  get(ns) {
+    return ns === 'electron' ? builtins.get(ns) : defaults.builtins.get(ns)
+  },
+  keys() {
+    return [...defaults.builtins.keys(), 'electron']
+  }
 }
 
-const mapImport = function (id) { return id }
+const mapImport = function (id) {
+  return id
+}
 
 const platform = {
   symbol: `platform-${defaults.symbol}`,
   protocol: 'holepunch',
-  map (id, { protocol, isImport, isBuiltin, isSourceMap, isConsole }) {
-    const type = isConsole ? protocol : (isSourceMap ? 'map' : isImport ? 'esm' : 'cjs')
+  map(id, { protocol, isImport, isBuiltin, isSourceMap, isConsole }) {
+    const type = isConsole
+      ? protocol
+      : isSourceMap
+        ? 'map'
+        : isImport
+          ? 'esm'
+          : 'cjs'
     return `${isBuiltin ? '/~' : ''}${encodeURI(id)}+${protocol}+${type}`
   },
   mapImport,
@@ -64,8 +109,14 @@ const platform = {
 const app = {
   symbol: defaults.symbol,
   protocol: defaults.protocol,
-  map (id, { protocol, isImport, isBuiltin, isSourceMap, isConsole }) {
-    const type = isConsole ? protocol : (isSourceMap ? 'map' : isImport ? 'esm' : 'cjs')
+  map(id, { protocol, isImport, isBuiltin, isSourceMap, isConsole }) {
+    const type = isConsole
+      ? protocol
+      : isSourceMap
+        ? 'map'
+        : isImport
+          ? 'esm'
+          : 'cjs'
     return `${isBuiltin ? '/~' : ''}${encodeURI(id)}+${protocol}+${type}`
   },
   mapImport,
